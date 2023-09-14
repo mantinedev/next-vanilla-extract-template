5c78135f167c905074f89f63605e9bd2893ed29a
Vitaly Rtishchev <rtivital@gmail.com>
2023-09-14 13:17:54 +0400

    Add viewport meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,12 @@ export default function RootLayout({ children }: { children: any }) {
     <html lang="en">
       <head>
         <ColorSchemeScript />
+        <title>Mantine Vanilla extract template</title>
         <link rel="shortcut icon" href="/favicon.svg" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
       </head>
       <body>
         <MantineProvider theme={theme}>{children}</MantineProvider>
