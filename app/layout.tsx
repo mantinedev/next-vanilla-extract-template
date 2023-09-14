157089dc416eee902193346f71b26ff77205f512
Oli Lalonde <olalonde@gmail.com>
2023-09-14 15:57:45 +0800

    class order is wrong in generated css file

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "@mantine/core/styles.css";
 import React from "react";
 import { MantineProvider, ColorSchemeScript } from "@mantine/core";
 import { theme } from "../theme";
+import { RedHeader } from "../components/RedHeader/RedHeader";
+// import { BlueHeader } from "../components/BlueHeader/BlueHeader";
 
 export const metadata = {
   title: "Mantine Next.js template",
@@ -16,7 +18,11 @@ export default function RootLayout({ children }: { children: any }) {
         <link rel="shortcut icon" href="/favicon.svg" />
       </head>
       <body>
-        <MantineProvider theme={theme}>{children}</MantineProvider>
+        <MantineProvider theme={theme}>
+          <RedHeader />
+          {/* <BlueHeader /> */}
+          {children}
+        </MantineProvider>
       </body>
     </html>
   );
---
+++ components/RedHeader/RedHeader.tsx
@@ -0,0 +1,7 @@
+'use client';
+import { Text } from '@mantine/core';
+import * as classes from './RedHeader.css';
+
+export function RedHeader() {
+  return <Text className={classes.red}>Red header?</Text>;
+}
